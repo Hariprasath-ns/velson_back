16e017e00ba030f3151d13ca08bc5f2eb0bdf5d8
Sujeeth-P <sujeeth9487@gmail.com>
2026-05-15 11:54:22 +0530

    base with reference master

--- src/middelwares/dbSelect.js
+++ src/middelwares/dbSelect.js
@@ -1,7 +1,10 @@
 import { neonPrisma, dockerPrisma } from "../config/db.js";
 
+const defaultDb = process.env.DB_ENV === "neon" ? "neon" : "docker";
+
 export const dbSelect = (req, res, next) => {
-  req.db = req.query.db === "neon" ? neonPrisma : dockerPrisma;
-  req.dbName = req.query.db === "neon" ? "neon" : "docker";
+  const db = req.query.db || defaultDb;
+  req.db = db === "neon" ? neonPrisma : dockerPrisma;
+  req.dbName = db === "neon" ? "neon" : "docker";
   next();
 };
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import { checkConnections } from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
+import referenceMasterRoute from "./routes/referenceMasterRoute.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", userRoute);
+app.use("/api", referenceMasterRoute);
 
 const PORT = process.env.PORT || 5000;
 
